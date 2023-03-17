39447b6b0a1aad06e2200e443c110033dfeb241c
marombal <miguel.marombal@gmail.com>
2023-03-17 16:13:06 +0000

    Process Orders Update

--- src/Order.java
+++ src/Order.java
@@ -79,18 +79,54 @@ public class Order {
         }
         else if(Objects.equals(WorkPiece, "P5")){
             // P2 - t3/10s -> P4 - t4/10s -> P7 - t3/20s -> P9 - t4/15s -> P5
+            time = Integer.parseInt(Quantity) * (10 + 10 + 20 + 15);
+            days = time / 60 + 1;
+            tool = "T3 + T4 + T3 + T4";
+
+            System.out.println("Raw material needed: P2, quantity: " + Quantity);
+            System.out.println("Transformations: P2 to P4 to P7 to P9 to P5 using " + tool);
+            System.out.println("Total time in machines: " + time + " seconds. (at least " + days + " days to complete)");
         }
         else if(Objects.equals(WorkPiece, "P6")){
+            // P1 - t1/20s -> P6
+            time = Integer.parseInt(Quantity) * (20);
+            days = time / 60 + 1;
+            tool = "T1";
+
+            System.out.println("Raw material needed: P1, quantity: " + Quantity);
+            System.out.println("Transformations: P1 to P6 using " + tool);
+            System.out.println("Total time in machines: " + time + " seconds. (at least " + days + " days to complete)");
 
         }
         else if(Objects.equals(WorkPiece, "P7")){
+            // P2 - t3/10s -> P4 - t4/10s -> P7
+            time = Integer.parseInt(Quantity) * (10 + 10);
+            days = time / 60 + 1;
+            tool = "T3 + T4";
 
+            System.out.println("Raw material needed: P2, quantity: " + Quantity);
+            System.out.println("Transformations: P2 to P4 to P7 using " + tool);
+            System.out.println("Total time in machines: " + time + " seconds. (at least " + days + " days to complete)");
         }
         else if(Objects.equals(WorkPiece, "P8")){
+            // P1 - t1/20s -> P6 - t3/30s -> P8
+            time = Integer.parseInt(Quantity) * (20 + 30);
+            days = time / 60 + 1;
+            tool = "T1 + T3";
 
+            System.out.println("Raw material needed: P1, quantity: " + Quantity);
+            System.out.println("Transformations: P1 to P6 to P8 using " + tool);
+            System.out.println("Total time in machines: " + time + " seconds. (at least " + days + " days to complete)");
         }
         else if(Objects.equals(WorkPiece, "P9")){
+            // P2 - t3/10s -> P4 - t4/10s -> P7 - t3/20s -> P9
+            time = Integer.parseInt(Quantity) * (10 + 10 + 20);
+            days = time / 60 + 1;
+            tool = "T3 + T4 + T3";
 
+            System.out.println("Raw material needed: P2, quantity: " + Quantity);
+            System.out.println("Transformations: P2 to P4 to P7 to P9 using " + tool);
+            System.out.println("Total time in machines: " + time + " seconds. (at least " + days + " days to complete)");
         }
     }
 }
